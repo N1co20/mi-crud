6aec7293640c2ff67814ff30e8d874bba1a19796
N1co20 <nikesayala11@gmail.com>
2026-07-09 12:18:28 -0400

    first commit

---
+++ src/components/Item.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Item({ item, deleteItem, editItem }) {
+  return (
+    <li>
+      <span>{item.value}</span>
+
+      <button onClick={() => editItem(item)}>Editar</button>
+      <button onClick={() => deleteItem(item.id)}>Eliminar</button>
+    </li>
+  );
+}
+
+export default Item;
